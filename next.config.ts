463e08a957196dec687e68a95f3378c571fe7edd
Alberto Maserati <alberto.maserati@outlook.com>
2026-04-11 00:04:55 +0200

    Rollback to routing only in middleware

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,15 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/en',
-        permanent: false,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       destination: '/en',
+  //       permanent: false,
+  //     },
+  //   ];
+  // },
 };
 
 const withNextIntl = createNextIntlPlugin();
